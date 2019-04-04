76e5f8dca77839c081207adb35dcefa4bf271aee
Chris Grayston <chrisgrayston@Chriss-MacBook-Pro-2.local>
2019-04-04 16:21:34 -0600

    Adds storyboards for all tabs in the tab bar. Adds Program VC, model object and model controller

---
+++ Workout/Controllers/Model Controllers/ProgramController.swift
@@ -0,0 +1,26 @@
+//
+//  ProgramController.swift
+//  Workout
+//
+//  Created by Chris Grayston on 4/4/19.
+//  Copyright © 2019 Chris Grayston. All rights reserved.
+//
+
+import Foundation
+
+class ProgramController {
+    // MARK: - Properties
+    var programs: [Program] = []
+    
+    // Source of truth
+    static let shared = ProgramController()
+    
+    // MARK: - CRUD Functions
+    
+    // Create Program
+    
+    // Update Program
+    
+    // Delete Program
+    
+}
